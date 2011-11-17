fac2560158be0b0e508f221710e3a0c833fa799e
Ben Symonds <ben@state.it>
2011-11-17 11:11:18 +0000

    use a mongoid observer and controller around_filter to pick up modifier from controller

--- lib/mongoid-history.rb
+++ lib/mongoid-history.rb
@@ -3,7 +3,9 @@ require 'easy_diff'
 require File.expand_path(File.dirname(__FILE__) + '/mongoid/history')
 require File.expand_path(File.dirname(__FILE__) + '/mongoid/history/tracker')
 require File.expand_path(File.dirname(__FILE__) + '/mongoid/history/trackable')
+require File.expand_path(File.dirname(__FILE__) + '/mongoid/history/sweeper')
 
 Mongoid::History.modifier_class_name = "User"
 Mongoid::History.trackable_classes = []
 Mongoid::History.trackable_class_options = {}
+Mongoid::History.current_user_method ||= :current_user
