534848fdcb0cbede90431c2d2ea3bd6fd3843596
Mattias Ekberg <mattias@mattiasekberg.com>
2013-01-12 22:31:34 +0100

    Added spec for test of multi relations to modifier. refs #11

---
+++ spec/multi_relation_spec.rb
@@ -0,0 +1,53 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe Mongoid::History::Tracker do
+  before :each do
+    
+    class Tracker
+      include Mongoid::History::Tracker
+    end
+    
+    class Model
+      include Mongoid::Document
+      include Mongoid::History::Trackable
+      
+      field :name, type: String
+      belongs_to :user, inverse_of: :models
+      has_and_belongs_to_many :external_users, class_name: "User", inverse_of: :external_models
+      
+      track_history   :on => :name,       # track title and body fields only, default is :all
+                  :modifier_field => :modifier, # adds "referenced_in :modifier" to track who made the change, default is :modifier
+                  :version_field => :version,   # adds "field :version, :type => Integer" to track current version, default is :version
+                  :track_create   =>  false,    # track document creation, default is false
+                  :track_update   =>  true,     # track document updates, default is true
+                  :track_destroy  =>  false    # track document destruction, default is false
+    end
+    
+    class User
+      include Mongoid::Document
+      has_many :models, :dependent => :destroy, inverse_of: :user
+      has_and_belongs_to_many :external_model, class_name: "Model", inverse_of: :external_users
+    end
+    
+    Mongoid::History.tracker_class_name == :tracker
+  end
+
+  after :each do
+    Mongoid::History.tracker_class_name = nil
+  end
+
+  it "should be possible to undo when having multiple relations to modifier class" do
+    user = User.new
+    user.save
+    
+    model = Model.new
+    model.name = "Foo"
+    model.user = user
+    model.save
+    
+    model.name = "Bar"
+    model.save
+    
+    model.undo! user
+  end
+end
