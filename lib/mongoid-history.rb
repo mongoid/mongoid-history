18dbc9cea7a0fde2b0038f16e32b6b682efb7d73
Johnny Shields <johnny.shields@gmail.com>
2014-01-11 14:30:24 +0900

    Cleanup: Move Version to a Ruby file

--- lib/mongoid-history.rb
+++ lib/mongoid-history.rb
@@ -1,8 +1,9 @@
 require 'easy_diff'
 
-require File.expand_path(File.dirname(__FILE__) + '/mongoid/history')
-require File.expand_path(File.dirname(__FILE__) + '/mongoid/history/tracker')
-require File.expand_path(File.dirname(__FILE__) + '/mongoid/history/trackable')
+require 'mongoid/history'
+require 'mongoid/history/version'
+require 'mongoid/history/tracker'
+require 'mongoid/history/trackable'
 
 Mongoid::History.modifier_class_name = "User"
 Mongoid::History.trackable_class_options = {}
---
+++ lib/mongoid/history/version.rb
@@ -0,0 +1,5 @@
+module Mongoid
+  module History
+    VERSION = '0.4.1'
+  end
+end
