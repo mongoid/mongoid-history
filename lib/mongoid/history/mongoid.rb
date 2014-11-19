0f4a8d487a7b77e436df4f623a58f3974bcbb035
Mehmet Yıldız <mehmet.iu@gmail.com>
2014-11-19 20:37:28 +0200

    Avoid the use of the case equality operator '==='.

--- lib/mongoid/history/mongoid.rb
+++ lib/mongoid/history/mongoid.rb
@@ -3,7 +3,7 @@ require 'versionomy'
 module Mongoid
   module History
     def self.mongoid3?
-      Versionomy.parse(::Mongoid::VERSION).major === 3
+      Versionomy.parse(::Mongoid::VERSION).major == 3
     end
   end
 end
