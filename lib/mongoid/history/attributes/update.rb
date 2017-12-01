66ce675222fa81039e6442ae1a99be269cc74365
Johnny Shields <johnny.shields@gmail.com>
2017-12-02 02:40:26 +0900

    Do not track unmodified relations

--- lib/mongoid/history/attributes/update.rb
+++ lib/mongoid/history/attributes/update.rb
@@ -10,7 +10,7 @@ module Mongoid
             elsif trackable_class.tracked_embeds_many?(k)
               insert_embeds_many_changes(k, v)
             elsif trackable_class.tracked?(k, :update)
-              @attributes[k] = format_field(k, v)
+              @attributes[k] = format_field(k, v) unless v.all?(&:blank?)
             end
           end
           @attributes
@@ -22,20 +22,22 @@ module Mongoid
           relation = trackable_class.database_field_name(relation)
           relation_class = trackable_class.embeds_one_class(relation)
           paranoia_field = Mongoid::History.trackable_class_settings(relation_class)[:paranoia_field]
-          @attributes[relation] = []
-          @attributes[relation][0] = value[0][paranoia_field].present? ? {} : format_embeds_one_relation(relation, value[0])
-          @attributes[relation][1] = value[1][paranoia_field].present? ? {} : format_embeds_one_relation(relation, value[1])
+          original_value = value[0][paranoia_field].present? ? {} : format_embeds_one_relation(relation, value[0])
+          modified_value = value[1][paranoia_field].present? ? {} : format_embeds_one_relation(relation, value[1])
+          return if original_value == modified_value
+          @attributes[relation] = [original_value, modified_value]
         end
 
         def insert_embeds_many_changes(relation, value)
           relation = trackable_class.database_field_name(relation)
           relation_class = trackable_class.embeds_many_class(relation)
           paranoia_field = Mongoid::History.trackable_class_settings(relation_class)[:paranoia_field]
-          @attributes[relation] = []
-          @attributes[relation][0] = value[0].reject { |rel| rel[paranoia_field].present? }
+          original_value = value[0].reject { |rel| rel[paranoia_field].present? }
                                              .map { |v_attrs| format_embeds_many_relation(relation, v_attrs) }
-          @attributes[relation][1] = value[1].reject { |rel| rel[paranoia_field].present? }
+          modified_value = value[1].reject { |rel| rel[paranoia_field].present? }
                                              .map { |v_attrs| format_embeds_many_relation(relation, v_attrs) }
+          return if original_value == modified_value
+          @attributes[relation] = [original_value, modified_value]
         end
       end
     end
